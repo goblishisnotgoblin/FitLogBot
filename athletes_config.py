cc0a497ba47c192565df91478cdb3d756bea6bdd
goblishisnotgoblin <dzts9rdd7v@privaterelay.appleid.com>
2025-12-04 16:07:21 +0100

    Add files via upload

---
+++ athletes_config.py
@@ -0,0 +1,6 @@
+ATHLETE_SHEETS = {
+    "Роман Г.": "1YKpW75xuGky8o7jj-uZ2gQVk2mKHyh_YD4b9z188fHs",
+    # добавишь сюда других людей:
+    # "Иван П.": "1AbCdEfGh123...",
+    # "Денис": "1QwErTyUiOp...",
+}
