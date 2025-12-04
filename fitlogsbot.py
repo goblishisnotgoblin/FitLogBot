21000f345dbd334060ce7cef5709fb51b4ec2cc8
goblishisnotgoblin <dzts9rdd7v@privaterelay.appleid.com>
2025-12-04 11:44:00 +0100

    Update fitlogsbot.py

--- fitlogsbot.py
+++ fitlogsbot.py
@@ -5,6 +5,9 @@ from aiogram import F
 from aiogram.types import Message
 
 import os
+from aiohttp import web
+import asyncio
+
 TOKEN = os.getenv("TOKEN")
 
 logging.basicConfig(level=logging.INFO)
@@ -22,9 +25,27 @@ dp = Dispatcher()
 async def handle_message(message: Message):
     await message.answer("Бот работает! Ты написал: " + message.text)
 
+
+async def start_webserver():
+    async def handle(request):
+        return web.Response(text="Bot is running")
+
+    app = web.Application()
+    app.add_routes([web.get("/", handle)])
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    port = int(os.getenv("PORT", 8080))
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+    logging.info(f"Web server started on port {port}")
+
+
 async def main():
+    asyncio.create_task(start_webserver())  # <-- добавили
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
